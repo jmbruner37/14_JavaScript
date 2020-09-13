3efd55bfcb61a293c62335c5abeb69dd117b1a9a
jmbruner37 <jmbruner37@gmail.com>
2020-09-12 20:17:39 -0700

    weather data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+//reference to table body
+var tbody = d3.select('tbody');
+
+// data.js console weather data
+console.log(data);
+
+ data.forEach(function(sightingReport) {
+    console.log(sightingReport);
+    var row = tbody.append("tr");
+     Object.entries(sightingReport).forEach(function([key, value]) {
+       console.log(key, value);
+       var cell = tbody.append("td");
+       cell.text(value);
+     });
+   });
+
